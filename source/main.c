afdbaccd0faf30c30fc3e112edd99ba7fb6d682f
amiddendorp22 <amiddendorp22@gmail.com>
2021-05-02 20:00:08 -0700

    Added links to video demos

--- source/main.c
+++ source/main.c
@@ -29,19 +29,10 @@ int main(void) {
 	ADC_init();
     while (1) {
 	unsigned short val = ADC;
-	unsigned short MAX = 1011;
-	if(val >= (MAX/2))
-	{
-		PORTB = 0x01;
-	}
-	else
-	{
-		PORTB = 0x00;
-	}
-	//unsigned char myVal = (char)val;
-	//PORTB = myVal >> 0x02; //grabs the upper eight bits from ADC
-	//unsigned char lowerBits = myVal & 0x03; //grabs the lower two bits from ADC
-	//PORTD = lowerBits;
+	unsigned char myVal = (char)val;
+	PORTB = myVal >> 0x02; //grabs the upper eight bits from ADC
+	unsigned char lowerBits = myVal & 0x03; //grabs the lower two bits from ADC
+	PORTD = lowerBits;
 
 	//lowest value for photoresistor: 194 (integer)
 	//highest value for photoresistor: 1011 (integer)
--- turnin/amidd001_lab7_part1.c
+++ turnin/amidd001_lab7_part1.c
@@ -1,9 +1,9 @@
-/*	Author: lab
+/*	Author: amidd001
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	Video Demo: https://youtu.be/XNvp3pH8wDE
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
